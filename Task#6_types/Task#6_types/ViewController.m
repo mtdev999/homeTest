7409095cb1c546dc93a623426915cd2b36d8b1b4
Mark Tezza <marktezza@gmail.com>
2015-11-09 13:30:58 +0200

    => added implementation to ViewController

--- Task#6_types/Task#6_types/ViewController.m
+++ Task#6_types/Task#6_types/ViewController.m
@@ -16,7 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // create parent rect with size 100x100 in points x=100 y=100
+    UIView *parentRect = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    [self.view addSubview:parentRect];
+    parentRect.backgroundColor = [UIColor greenColor];
+    
+    // create new rect(size 40x40 points (30,30)) inside parent's rect
+    CGRect newRect = CGRectMake(30, 30, 40, 40);
+
+    UIView *newView = [[UIView alloc] initWithFrame:newRect];
+    [parentRect addSubview:newView];
+    newView.backgroundColor = [UIColor blueColor];
+    
+    // create random points
+    CGPoint point;
+    
+    for (NSUInteger index = 0 ; index < 50; index++) {
+        point.x = arc4random_uniform(100) + 30;
+        point.y = arc4random_uniform(100) + 30;
+        
+//        point.x = arc4random() % 101;
+//        point.y = arc4random() % 101;
+        
+        point = CGPointMake(point.x, point.y);
+        
+        BOOL result = CGRectContainsPoint(newRect, point);
+        if (result == 1) {
+            NSLog(@" %lu.result = %@, x = %f y = %f" ,(unsigned long)index, result ? @"YES":@"NO", point.x, point.y);
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning {
