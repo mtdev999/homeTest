2ba05478055c30b6069fb0958694ae0d214f3d8f
Mark Tezza <marktezza@gmail.com>
2015-11-09 10:02:37 +0200

    added task#6

---
+++ Task#6_types/Task#6_types/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task#6_types
+//
+//  Created by Mark Tezza on 09.11.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
