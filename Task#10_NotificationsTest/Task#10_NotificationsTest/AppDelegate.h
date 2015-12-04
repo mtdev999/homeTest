4ab2c2b0d7617ffe02b067fe6badb3c730983c3b
Mark Tezza <marktezza@gmail.com>
2015-12-04 13:51:02 +0200

    added new task 'Notifications Test'

---
+++ Task#10_NotificationsTest/Task#10_NotificationsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task#10_NotificationsTest
+//
+//  Created by Mark Tezza on 04.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task#10_NotificationsTest/Task#10_NotificationsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task#10_NotificationsTest
+//
+//  Created by Mark Tezza on 04.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
