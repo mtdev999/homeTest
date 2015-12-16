bc3ad3d90154500e044022e5cf9c8f2af2614543
Mark Tezza <marktezza@gmail.com>
2015-12-16 14:33:04 +0200

    level 'pupil' ready for use

--- task#12_blockTest/task#12_blockTest/AppDelegate.m
+++ task#12_blockTest/task#12_blockTest/AppDelegate.m
@@ -16,30 +16,53 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"*********Level Pupil*********");
+    
+    // void block
+    void(^block)(void) = ^{
+        NSLog(@"test Block");
+    };
+    
+    block();
+    
+    // block with parametr
+    void(^blockWithParam)(NSString *) = ^(NSString *string) {
+        NSLog(@"test block with parametr: string - '%@'", string);
+    };
+
+    blockWithParam(@"string");
+    blockWithParam(@"string1");
+    blockWithParam(@"string2");
+   
+    // block with parametrs
+    void(^blockWithParametrs)(NSString *, NSUInteger) = ^(NSString *string, NSUInteger intValue){
+        NSLog(@"test block with parametrs: string - '%@', intValue = '%lu'", string, intValue);
+    };
+    
+    blockWithParametrs(@"string1", 111);
+    blockWithParametrs(@"string2", 222);
+    blockWithParametrs(@"string3", 333);
+    
+    // block with type and parametr
+    NSString *(^blockWithReturnValueWithParametrs)(NSString*,NSUInteger) = ^(NSString *string2, NSUInteger intValue2) {
+        return [NSString stringWithFormat:@"block return value with parametrs: %@, %lu", string2, intValue2];
+    };
+    
+    NSString *result = blockWithReturnValueWithParametrs(@"string NEW", 999);
+    NSLog(@"%@", result);
+    
+    [self testBlockWithParameter:^{
+        NSLog(@"Block was called");
+    }];
+    
+    NSLog(@"*********Level Student********");
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)testBlockWithParameter:(void (^)(void))testBlock {
+    NSLog(@"Here will be call block now!!!");
+    testBlock();
 }
 
 @end
