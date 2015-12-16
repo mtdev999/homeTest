e04739917120ab42841cbc1ed7ddc49a9be7bc25
Mark Tezza <marktezza@gmail.com>
2015-12-16 12:47:09 +0200

    added new task#12_blockTest

---
+++ task#12_blockTest/task#12_blockTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  task#12_blockTest
+//
+//  Created by Mark Tezza on 16.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ task#12_blockTest/task#12_blockTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  task#12_blockTest
+//
+//  Created by Mark Tezza on 16.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
