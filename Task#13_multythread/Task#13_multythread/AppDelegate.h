0284fcb3aa86d16562d499fa7b739c9a159816c8
Mark Tezza <marktezza@gmail.com>
2015-12-19 13:39:57 +0200

    adde new task 'multithread'

---
+++ Task#13_multythread/Task#13_multythread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task#13_multythread
+//
+//  Created by Mark Tezza on 19.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task#13_multythread/Task#13_multythread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task#13_multythread
+//
+//  Created by Mark Tezza on 19.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
