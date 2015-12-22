662d5c96227ce1b04441e7ee5ef7e94ce5637e5f
Mark Tezza <marktezza@gmail.com>
2015-12-22 14:13:27 +0200

    added new task 'NSDateTest'

---
+++ Task#16_NSDateTest/Task#16_NSDateTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task#16_NSDateTest
+//
+//  Created by Mark Tezza on 22.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task#16_NSDateTest/Task#16_NSDateTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task#16_NSDateTest
+//
+//  Created by Mark Tezza on 22.12.15.
+//  Copyright © 2015 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
