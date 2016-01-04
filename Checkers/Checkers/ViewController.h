6445f8de1bd636cdc0de315b9c1ca506ff74c4e1
Mark Tezza <marktezza@gmail.com>
2016-01-04 15:42:32 +0200

    added new project 'checkers'

---
+++ Checkers/Checkers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Checkers
+//
+//  Created by Mark Tezza on 04.01.16.
+//  Copyright © 2016 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
