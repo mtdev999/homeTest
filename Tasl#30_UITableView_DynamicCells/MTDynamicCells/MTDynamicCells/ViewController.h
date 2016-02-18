ecc5269613c1f88e53b5f4af68a0d11b0cf15e69
Mark Tezza <marktezza@gmail.com>
2016-02-18 09:23:33 +0200

    added new task 'dynamic cells'

---
+++ Tasl#30_UITableView_DynamicCells/MTDynamicCells/MTDynamicCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTDynamicCells
+//
+//  Created by Mark Tezza on 18.02.16.
+//  Copyright © 2016 Mark Tezza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
